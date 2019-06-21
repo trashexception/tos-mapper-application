0a15e04af9b45a197f908225ebd7a52e9fb6a0bd
TrashBong <helpalee@gmail.com>
2019-06-22 03:08:26 +0900

    modify some UI; code Refactoring;

--- main.js
+++ main.js
@@ -1,37 +1,58 @@
-const { app, BrowserWindow } = require('electron')
-var child = require('child_process').spawn;
-var process = require('process');
-var fs = require('fs');
-//var _ = require('lodash');
-//var deasync = require('deasync');
-var psnode = require('ps-node');
-var executablePath = app.getAppPath() + "/mapper/tos-mapper.exe";
-var parameters = ["--user.x=1130", "--user.y=27", "--user.width=300", "--user.height=31", "--user.tessdata=" + app.getAppPath() + "/mapper/tessdata"];
-var childProcesser;
+const { app, BrowserWindow } = require('electron');
+const mapper_parametersFile = require(app.getAppPath() + "/res/json/mapper_parameters.json");
+const child = require('child_process').spawn;
+const process = require('process');
+const fs = require('fs');
+const isRunning = require('is-running')
+
+const executablePath = app.getAppPath() + "/mapper/tos-mapper.exe";
+const applicationPidFile = app.getAppPath() + "/mapper/application.pid";
 
 // window 객체는 전역 변수로 유지. 이렇게 하지 않으면, 
 // 자바스크립트 객체가 가비지 콜렉트될 때 자동으로 창이 닫힐 것입니다.
-let win
+let win;
+let childProcesser;;
 
-function createWindow() {
-  childProcesser = child(executablePath, parameters);
-  
-  console.log('tt: ' +childProcesser.pid);
+function processRun() {
+  // let mapper_parameters = ["--user.x=848", "--user.y=26", "--user.width=232", "--user.height=31", "--user.tessdata=" + app.getAppPath() + "\\mapper\\tessdata"];
+  let mapper_parameters = [
+
+    "--user.x=" + mapper_parametersFile.x,
+    "--user.y=" + mapper_parametersFile.y,
+    "--user.width=" + mapper_parametersFile.width,
+    "--user.height=" + mapper_parametersFile.height,
+    "--user.tessdata=" + app.getAppPath() + mapper_parametersFile.tessdata
+
+  ];
+  console.log("mapper_parameters ->", mapper_parameters);
+
+  childProcesser = child(executablePath, mapper_parameters);
+  console.log("childProcesser.pid -> ", childProcesser.pid);
+
   while (true) {
-    if (fs.existsSync(app.getAppPath() + "/mapper/application.pid")) {
+    if (fs.existsSync(applicationPidFile)) {
+      let jvmPid = fs.readFileSync(applicationPidFile, 'utf8');
+      if (isRunning(jvmPid)) {
+        fs.unlinkSync(applicationPidFile);
+        break;
+      }
+    }
+    if (childProcesser.pid == undefined) {
+
       break;
     }
-    if (childProcesser.pid == undefined)
-      break;
-
   }
-  if (childProcesser.pid == undefined){
+  if (childProcesser.pid == undefined) {
     console.log('ERRORORROROROR');
   }
+}
+
+function createWindow() {
+
   // 브라우저 창을 생성합니다.
   win = new BrowserWindow({
-    width: 1280,
-    height: 1024,
+    width: 1220,
+    height: 1030,
 
     webPreferences: {
       nodeIntegration: true,
@@ -39,37 +60,23 @@ function createWindow() {
     }
   });
 
-  // and load the index.html of the app.
-  win.loadFile('index.html');
-  // 개발자 도구를 엽니다.
-  win.webContents.openDevTools();
 
-  // psnode.lookup({
-  //   command: 'java',
-  //   arguments: 'tessdata',
-  // }, function (err, resultList) {
-  //   if (err) {
-  //     throw new Error(err);
-  //   }
-  //   resultList.forEach(function (process) {
-  //     if (process) {
-  //       console.log('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
-  //       ppid = process.pid;
-  //     }
-  //   });
-  // });
+  win.loadFile("./res/html/main.html"); // and load the index.html of the app.
+  win.setMenu(null);  // 메뉴창 제거
+
+  //win.webContents.openDevTools();   // 개발자 도구를 엽니다.
 
   win.on('close', function (e) { //   <---- Catch close event
     e.preventDefault();
-    console.log(app.getAppPath() + "/mapper/application.pid");
-    if (fs.existsSync(app.getAppPath() + "/mapper/application.pid")){
-      var data = fs.readFileSync(app.getAppPath() + "/mapper/application.pid",'utf8');
+    console.log("applicationPidFile Path ->", applicationPidFile);
+    if (fs.existsSync(applicationPidFile)) {
+      let jvmPid = fs.readFileSync(applicationPidFile, 'utf8');
       // the data is passed to the callback in the second argument
-      console.log(data);
-      process.kill(data,'SIGKILL');
-      fs.unlinkSync(app.getAppPath() + "/mapper/application.pid");
+      console.log("jvmPid -> ", jvmPid);
+      process.kill(jvmPid, 'SIGKILL');
+      fs.unlinkSync(applicationPidFile);
     }
-    win.loadFile('shutdown.html');
+    win.loadFile('./res/html/shutdown.html');
     setTimeout(function () {
       win.destroy();
     }, 2000);
@@ -80,56 +87,26 @@ function createWindow() {
   win.on('closed', () => {
     // window 객체에 대한 참조해제. 여러 개의 창을 지원하는 앱이라면 
     // 창을 배열에 저장할 수 있습니다. 이곳은 관련 요소를 삭제하기에 좋은 장소입니다.
-    // console.log('TTTTTTT : ' + ppid);
-    // if (ppid != -1){
-    //   psnode.kill(ppid,'SIGKILL',function(err){
-    //     if (err) {
-    //       throw new Error( err );
-    //     }
-    //     else {
-    //       console.log( 'Process %s has been killed!', pid );
-    //     }
-    //   });
 
-    // childProcesser.stdin.end();
-    // childProcesser.stdout.destroy();
-    // childProcesser.stderr.destroy();
-    // childProcesser.kill(-childProcesser.pid);
-    console.log('window closed');
     win = null;
   }
   );
 
-
-
-
-
-  win.setMenu(null);
 }
 
 // 이 메서드는 Electron이 초기화를 마치고 
 // 브라우저 창을 생성할 준비가 되었을 때  호출될 것입니다.
 // 어떤 API는 이 이벤트가 나타난 이후에만 사용할 수 있습니다.
-app.on('ready', createWindow)
+app.on('ready', () => {
+
+  processRun();
+  createWindow();
+
+});
 
 app.on('will-quit', () => {
-  // // win.loadFile('shutdown.html');
-  // console.log('will quit end');
-  // var request = require('sync-request');
-  // var res = request('GET', 'http://localhost:31234/api/shutdown', {
-  //   timeout: 1000,
-  //   socketTimeout: 1000,
-  //   retry: false,
-  // });
-  // console.dir(res);
-  // console.log(res.getBody());
-  // childProcesser.kill();
-  console.log('will quit real end');
-})
-
-
-
-
+  //종료 직전에 수행되는 HOOK
+});
 
 // 모든 창이 닫혔을 때 종료.
 app.on('window-all-closed', () => {
@@ -139,6 +116,7 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 })
+
 app.on('activate', () => {
   // macOS에서는 dock 아이콘이 클릭되고 다른 윈도우가 열려있지 않았다면
   // 앱에서 새로운 창을 다시 여는 것이 일반적입니다.
